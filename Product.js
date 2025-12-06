6ef26c33bb827f796e80270422f96f619d4a07d0
avidersonline-ux <avidersonline@gmail.com>
2025-12-06 18:30:40 +0530

    Create Product.js

---
+++ Product.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  title: String,
+  brand: String,
+  image: String,
+  price: Number,
+  mrp: Number,
+  currency: String,
+  category: String,
+  source: String,
+  affiliateUrl: String,
+  updated_at: Date,
+});
+
+export default mongoose.model("Product", ProductSchema);
+
