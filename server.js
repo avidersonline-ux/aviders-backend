4365b146224168d29545de9851f68d4caf6879f7
avidersonline-ux <avidersonline@gmail.com>
2025-12-29 00:15:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import categoriesRoute from "./routes/categories.js";
 import priceHistoryRoute from "./routes/price_history.js";
 import basketProductsRoutes from "./routes/basket_products.js";
 import basketCategories from "./routes/basketCategories.js";
-import shopcategories from "./routes/shopcategories.js";
+
 
 
 dotenv.config();
@@ -30,7 +30,7 @@ app.use("/categories", categoriesRoute);
 app.use("/price-history", priceHistoryRoute);
 app.use("/basket-products", basketProductsRoutes);
 app.use("/basket-categories", basketCategories);
-app.use("/shopcategories", shopCategories);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
